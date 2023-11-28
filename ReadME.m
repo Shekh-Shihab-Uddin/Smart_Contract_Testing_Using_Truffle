d364a06d30915556dffbc9b14d8fa174fc8edc3d
Shekh-Shihab-Uddin <126742168+Shekh-Shihab-Uddin@users.noreply.github.com>
2023-11-28 09:25:27 +0600

    Update ReadME.m

--- ReadME.m
+++ ReadME.m
@@ -1,6 +1,11 @@
+Created a unique #energy #trading  #smartcontract using solidity. The main manager of this system is the owner or deployer. Only or any agent set by him can register deregister any producer and consumer. Then the producer and the consumer can transact energy between them with the exchange of #ether .
+— First created and tested the contract in #remix IDE
+— Then Performed the #smartcontract #testing using #truffle framework's contract() method. Took the help of the account from local test net #ganache and also deployed on it
+
 Just clone this repository.
 - npm install -g
 - npm install -g truffle
 - truffle test (for testing the smartcontract)
 - truffle migrate --reset (for deploying the smartcontract on local blockchain)
 (For deploying on test-net or live net configure the truffle-config.js file)
+
