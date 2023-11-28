fa4093771fb4a07ded252fc6e20214eef7c62ebc
Shekh-Shihab-Uddin <126742168+Shekh-Shihab-Uddin@users.noreply.github.com>
2023-11-28 08:58:40 +0600

    Create ReadME.m

---
+++ ReadME.m
@@ -0,0 +1,6 @@
+Just clone this repository.
+- npm install -g
+- npm install -g truffle
+- truffle test (for testing the smartcontract)
+- truffle migrate --reset (for deploying the smartcontract on local blockchain)
+(For deploying on test-net or live net configure the truffle-config.js file)
